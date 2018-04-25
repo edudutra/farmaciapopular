d8b3ed6ab00cdbbe44c00968a802c13832066b36
Eduardo Dutra <eduardo.dutra@habibs.com.br>
2018-04-25 15:44:57 -0300

    sleep to avoid block

--- carga/update_geo.js
+++ carga/update_geo.js
@@ -1,8 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
 var NodeGeocoder = require('node-geocoder');
 
+var HttpsAdapter = require('node-geocoder/lib/httpadapter/httpsadapter.js')
+var httpAdapter = new HttpsAdapter(null, {
+    headers: {
+        'user-agent': 'Farmacia Popular Bot <emdutra@gmail.com>'
+    }
+});
+
 var options = {
-    provider: 'openstreetmap'
+    provider: 'openstreetmap',
+    httpAdapter: httpAdapter
 };
 
 var geocoder = NodeGeocoder(options);
@@ -10,9 +18,9 @@ var geocoder = NodeGeocoder(options);
 
 const url = 'mongodb+srv://edudutra:Reuel-Staples@cluster0-slco2.mongodb.net/admin';
 const dbName = 'farmaciapopular';
+let client;
 
 (async function() {
-    let client;
 
     try {
         client = await MongoClient.connect(url);
@@ -28,16 +36,52 @@ const dbName = 'farmaciapopular';
 //        assert.equal(3, r.insertedCount);
 
         // Get the cursor
-        const cursor = col.find({ localizacao : { $exists:0 } }).limit(10);
+        const cursor = col.find({$and : [{ notfound : { $exists : 0 } }, {localizacao : {$exists : 0}}]}).sort([["cnpj_da_farmacia", 1]]).limit(99999); //, uf : "SP"}).limit(1000);
 
         // Iterate over the cursor
         while(await cursor.hasNext()) {
             const doc = await cursor.next();
-            console.dir(doc);
+            console.log({cnpj: doc.cnpj_da_farmacia, nome: doc.farmacia, endereco: doc.endereco});
 
-            geocoder.geocode({q: doc.endereco + ', ' + doc.bairro + ', ' + doc.municipio + ', ' + doc.uf + ', Brazil'} , function(err, res) {
-                console.log(res);
+            var query = {
+                format: 'json',
+                q: doc.endereco + ', ' + doc.municipio + ', ' + doc.uf + ', Brazil'
+                }
+
+            geocoder.geocode(query, function(err, res) {
+                if (err) {
+                    console.log(err.message)
+                    return
+                }
+                if (res && Array.isArray(res) && res.length > 0) {
+                    //console.log(res[0])
+                    col.updateOne({_id : doc._id }, { $set : { localizacao :
+                                {
+                                    type: "Point",
+                                    coordinates: [res[0].longitude, res[0].latitude]
+                                }
+                    }}
+                        , function(err, r) {
+                            if (err) console.log(err)
+                            console.log(doc.cnpj_da_farmacia + '\t' + doc.farmacia + ': ' + r.result.ok)
+                            //if (!cursor.hasNext()) client.close()
+                        })
+                }
+                else {
+                    //console.log(res[0])
+                    col.updateOne({_id : doc._id }, { $set : { notfound : [options.provider] }},
+                        function(err, r) {
+                            if (err) console.log(err)
+                            console.log('Not Found:\t' + doc.cnpj_da_farmacia + '\t' + doc.farmacia + ': ' + r.result.ok)
+                            //if (!cursor.hasNext()) client.close()
+                        })
+
+                    //console.log(doc.cnpj_da_farmacia + '\t' + doc.farmacia + ': Not Found')
+                    //if (!cursor.hasNext()) client.close()
+                }
+
             });
+            await sleep(1000)
 
         }
     } catch (err) {
@@ -45,5 +89,11 @@ const dbName = 'farmaciapopular';
     }
 
     // Close connection
-    client.close();
-})();
+    //client.close();
+})()
+
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
